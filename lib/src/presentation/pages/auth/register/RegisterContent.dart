e229bbe02f6c721e663d4a78880d6b5db4932e66
Mario-Fernando24 <3115408069.mafe@gmail.com>
2025-07-06 16:29:08 -0500

    ui registro stack

--- lib/src/presentation/pages/auth/register/RegisterContent.dart
+++ lib/src/presentation/pages/auth/register/RegisterContent.dart
@@ -10,8 +10,97 @@ class RegisterContent extends StatefulWidget {
 class _RegisterContentState extends State<RegisterContent> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Registro'),
+    return Stack(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.only(left: 12.0),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                  Color.fromARGB(255, 12, 38, 145),
+                  Color.fromARGB(255, 34, 156, 249),
+               ]
+              )
+          ), 
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _textLoginRotated(context),
+              SizedBox(height: 100),
+              _textRegisterRotated(context),
+              SizedBox(height: MediaQuery.of(context).size.height*0.25),
+
+            ],
+          ),
+        ),
+
+        Container(
+          margin: EdgeInsets.only(left: 60, bottom: 35),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              bottomLeft: Radius.circular(35)
+            ),
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromARGB(255, 189, 189, 189),
+                Color.fromARGB(255, 245, 245, 245),
+              ]
+             ),
+          ),
+          child: _imageBanner(),
+        )
+      ],
+    );
+  }
+
+    Widget _textLoginRotated(BuildContext context) {
+    return RotatedBox(
+      quarterTurns: 1,
+      child: Text(
+        'Login',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 27,
+        ),
+      ),
+    );
+  }
+
+  Widget _textRegisterRotated(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, 'register'),
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Text(
+          'Registro',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 27,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _imageBanner(){
+    return Container(
+      margin: EdgeInsets.only(top: 60),
+      alignment: Alignment.center,
+      child: Image.asset(
+        'assets/img/car_white.png',
+        width: 200,
+        height: 200,
+        ),
     );
   }
 }
